2c01ffbc665de0c1d18d0b66dd050c4d888455f7
Ariel Grubert <ariel.pd@hotmail.com>
2015-12-22 17:51:11 -0200

    pdf

--- app/controllers/pop_controller.rb
+++ app/controllers/pop_controller.rb
@@ -4,16 +4,19 @@ class PopController < ApplicationController
    	:pdf => 'application/pdf'
    }
 
-
 	def index
 	end
 
 	def show
+		@pop = OpenStruct.new(params)
 		respond_to do |format|
 	      format.html
 	      format.pdf do
-	        render pdf: "pop", 
-	        			template: 'pop/pop.pdf.erb'
+	        render pdf: "pop - #{I18n.transliterate(@pop.nome)}", 
+	        			template: 'pop/pop.pdf.erb',
+	        			background: true,
+	        			page_size: 'A4',
+	        			zoom: 1
 	      end
 	   end				
 	end
